406d1801454f8e3d4dc5f529a777f20b514d35c3
Dante <tran@Dantes-MacBook-Air.local>
2025-01-21 11:28:11 +0700

    feat: Add f16

---
+++ f1/1800. Maximum Ascending Subarray Sum.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxAscendingSum(self, nums: List[int]) -> int:
+        result = 0
+        cur = nums[0]
+
+        for i in range(len(nums) - 1):
+
+            if nums[i] < nums[i + 1]:
+                cur += nums[i + 1]
+            else:
+                result = max(cur, result)
+                cur = nums[i + 1]
+
+        return max(cur, result)
